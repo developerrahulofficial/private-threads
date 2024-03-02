5fca6bf2ad53cce80a2ae1474f5bd248cd0b2349
Developer Rahul <dboy1091997@gmail.com>
2024-03-02 17:00:26 +0530

    Update qr-code.tsx

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -12,9 +12,9 @@ export default function QRcode() {
             <div className="fixed right-2 bottom-2 lg:right-10 lg:bottom-10 flex flex-col justify-center items-center gap-5 z-50  scale-75 lg:scale-100">
                 <span className="text-[#777777] text-[13px] tracking-wide">Scan to get the code</span>
                 <div className='hover:scale-105 transform active:scale-95 transition-transform select-none '>
-                    <Zoom>
+                    {/* <Zoom>
                         <QRCodeSvg className=' w-[175px] h-[175px]  p-1 cursor-pointer border border-[#393939] z-50 rounded-2xl bg-[#181818] ' />
-                    </Zoom>
+                    </Zoom> */}
                 </div>
             </div>
         )
